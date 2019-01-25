35e633bf9f0fc1b61d95edf1a8c07808bcbb860d
MERKATOR <MERKATOR@DESKTOP-IKG681A>
2019-01-25 21:18:34 +0300

    SumUnit added SumV3 method;

--- CodeForces/CodeForces/Units/SumUnit.cs
+++ CodeForces/CodeForces/Units/SumUnit.cs
@@ -8,15 +8,26 @@ namespace CodeForces.Units
 		{
 			Console.WriteLine(SumV1(100));
 			Console.WriteLine(SumV2(100));
+			Console.WriteLine(SumV3(100));
 		}
 		private static Int32 SumV1(Int32 n)
+		{
+			var result = 0;
+			for(var i = 0; i <= n; i++)
+			{
+				result = result + i;
+			}
+
+			return result;
+		}
+		private static Int32 SumV2(Int32 n)
 		{
 			if(n <= 1)
 				return 1;
 
-			return n + SumV1(n - 1);
+			return n + SumV2(n - 1);
 		}
-		private static Int32 SumV2(Int32 n)
+		private static Int32 SumV3(Int32 n)
 		{
 			return (n * (n + 1)) / 2;
 		}
