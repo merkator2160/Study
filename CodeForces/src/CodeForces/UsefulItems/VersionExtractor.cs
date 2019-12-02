90598403662229c16e0d9e78388ba716ee592be2
MERKATOR <MERKATOR@DESKTOP-G8UCIOV>
2019-12-03 01:25:41 +0300

    Removed Utils.VersionExtractor;

---
+++ CodeForces/src/CodeForces/UsefulItems/VersionExtractor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace CodeForces.UsefulItems
+{
+	public static class VersionExtractor
+	{
+		public static String Extract(String extractionTargetFileName)
+		{
+			var currentDirectory = Directory.GetCurrentDirectory();
+			var files = Directory.GetFiles(currentDirectory);
+			var firstExecutable = files.FirstOrDefault(p => p.Contains(extractionTargetFileName));
+
+			if(firstExecutable == null)
+				throw new FileNotFoundException($"File: {extractionTargetFileName} was not found!");
+
+			return FileVersionInfo.GetVersionInfo(firstExecutable).FileVersion;
+		}
+	}
+}
