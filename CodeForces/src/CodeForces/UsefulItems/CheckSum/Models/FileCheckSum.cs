a2d517ab8bffcd9fc67b8a671d10dace5436a8d1
MERKATOR <MERKATOR@DESKTOP-G8UCIOV>
2019-12-03 01:32:53 +0300

    Removed CheckSumCulculator; Removed Common; Removed Updater;

---
+++ CodeForces/src/CodeForces/UsefulItems/CheckSum/Models/FileCheckSum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CodeForces.UsefulItems.CheckSum.Models
+{
+	public class FileCheckSum
+	{
+		public String FileName { get; set; }
+		public String CheckSum { get; set; }
+	}
+}
